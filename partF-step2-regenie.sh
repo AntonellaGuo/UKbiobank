bbd8fe38535de0820803bdb3cbc57ea0ad0ab0be
AntonellaGuo <guositong@nibs.ac.cn>
2025-10-29 16:01:58 +0800

    Update

--- partF-step2-regenie.sh
+++ partF-step2-regenie.sh
@@ -17,9 +17,9 @@
 # note that if you have multiple phenotypes, you will have a .regenie.gz for each phenotype
 # - /Data/assoc.c1.log  - regenie log for chromosome 1
 
-$project=$1
-exome_file_dir="${project}:/Bulk/Exome sequences/Population level exome OQFE variants, PLINK format - final release/"
-data_field="23155"
+project=$1
+exome_file_dir="${project}:/Bulk/Exome sequences/Population level exome OQFE variants, PLINK format - final release"
+data_field="23158"
 data_file_dir="${project}:/Data/"
 
 
@@ -31,15 +31,20 @@ for chr in {1..22}; do
     --phenoCol urticaria_cc --covarCol age --covarCol sex --covarCol ever_smoked \
     --pred urticaria_results_pred.list --bsize 200\
     --pThresh 0.05 --minMAC 3 --threads 16 --gz"
+    
+  dx run app-swiss-army-knife \
+    -iin="${exome_file_dir}/ukb${data_field}_c${chr}_b0_v1.bed" \
+    -iin="${exome_file_dir}/ukb${data_field}_c${chr}_b0_v1.bim" \
+    -iin="${exome_file_dir}/ukb${data_field}_c${chr}_b0_v1.fam" \
+    -iin="${data_file_dir}/WES_c${chr}_snps_qc_pass.snplist" \
+    -iin="${data_file_dir}/urticaria_wes.phe" \
+    -iin="${data_file_dir}/urticaria_results_pred.list" \
+    -iin="${data_file_dir}/urticaria_results_1.loco.gz" \
+    -icmd="${run_regenie_cmd}" \
+    --tag="Step2" \
+    --instance-type "mem1_ssd1_v2_x16" \
+    --destination="${project}:/Data/" \
+    --brief \
+    --yes
 
-  dx run swiss-army-knife \
-   -iin="${exome_file_dir}/ukb${data_field}_c${chr}_b0_v1.bed" \ #bed file for chromosome
-   -iin="${exome_file_dir}/ukb${data_field}_c${chr}_b0_v1.bim" \ #bim file for chromosome
-   -iin="${exome_file_dir}/ukb2${data_field}_c${chr}_b0_v1.fam"\ #fam file for chromosome
-   -iin="${data_file_dir}/WES_c${chr}_snps_qc_pass.snplist"\ #individual QC snplist produced with 04-step2-qc-filter.sh
-   -iin="${data_file_dir}/urticaria_wes.phe" \
-   -iin="${data_file_dir}/urticaria_results_pred.list" \ #Merged results produced from 03-step1-regenie.sh
-   -iin="${data_file_dir}/urticaria_results_1.loco.gz" \ #Merged results produced from 03-step1-regenie.sh
-   -icmd="${run_regenie_cmd}" --tag="Step2" --instance-type "mem1_ssd1_v2_x16"\
-   --destination="${project}:/Data/" --brief --yes
 done
