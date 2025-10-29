910b98dcf70fe02b0068400a1e8ac4efc758b5c6
AntonellaGuo <guositong@nibs.ac.cn>
2025-10-29 15:27:17 +0800

    Update

--- partE-step2-qc-filter.sh
+++ partE-step2-qc-filter.sh
@@ -19,7 +19,7 @@
 
 #set this to the exome sequence directory that you want (should contain PLINK formatted files)
 exome_file_dir="/Bulk/Exome sequences/Population level exome OQFE variants, PLINK format - final release"
-data_field="ukb23158"
+data_field="23158"
 data_file_dir="/Data/"
 
 for i in {1..22}; do
@@ -30,9 +30,9 @@ for i in {1..22}; do
       --out WES_c${i}_snps_qc_pass"
 
     dx run swiss-army-knife \
-     -iin="${exome_file_dir}/${data_field}_c${i}_b0_v1.bed" \
-     -iin="${exome_file_dir}/${data_field}_c${i}_b0_v1.bim" \
-     -iin="${exome_file_dir}/${data_field}_c${i}_b0_v1.fam" \
+     -iin="${exome_file_dir}/ukb${data_field}_c${i}_b0_v1.bed" \
+     -iin="${exome_file_dir}/ukb${data_field}_c${i}_b0_v1.bim" \
+     -iin="${exome_file_dir}/ukb${data_field}_c${i}_b0_v1.fam" \
      -iin="${data_file_dir}/urticaria_wes.phe" \
      -icmd="${run_plink_wes}" --tag="Step2" --instance-type "mem1_ssd1_v2_x16"\
      --destination="${project}:/Data/" --brief --yes
